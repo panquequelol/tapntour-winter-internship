70d3dd9d183bc90b08042d38e5d8801f8589e4c9
panquequelol <rene@outlook.my>
2022-12-30 21:57:55 -0300

    feat: display all guides

--- src/stores/guide.js
+++ src/stores/guide.js
@@ -1,4 +1,35 @@
-import { defineStore } from 'pinia'
+import { defineStore } from "pinia";
+import axios from "axios";
 
-const guide_endpoint = 'https://tapntour-wk-db.glyfo.workers.dev/api/fetchAllGuides'
+const guide_endpoint = "https://tapntour-wk-db.glyfo.workers.dev/api/guides";
 
+export const useGuideStore = defineStore({
+  id: "guide",
+  state: () => ({
+    guides: [],
+    guide: null,
+    loading: false,
+    error: null,
+  }),
+  actions: {
+    async fetchGuides() {
+      this.guides = [];
+      this.loading = true;
+      try {
+        const { data } = await axios.post(guide_endpoint, {
+          mode: "no-cors",
+          headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Content-Type": "application/json",
+          },
+        });
+        console.log("[guide store]: fetch data");
+        this.guides = data;
+      } catch (error) {
+        this.error = error;
+      } finally {
+        this.loading = false;
+      }
+    },
+  },
+});
